066a88b0d2fb6d2f3cf6fcf5ee0fbf70a2f9d8e6
ahang <hang.andy.t@gmail.com>
2018-03-23 17:13:04 -0500

    react stuff

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyAq2DnU_ImuKzbH4wTolQ52CkoNeceBsqo";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return(
+    <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
