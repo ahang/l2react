a3cc86c69bf5acbbf9d26a02a0dfd00db7b21ec7
Andy Hang <andy@llt-group.com>
2018-03-26 10:35:30 -0500

    renamed folders for ease of access.

---
+++ Section_5-Redux-Weather/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a6d62a9b3cffbbfb6b2659bccca93091';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
